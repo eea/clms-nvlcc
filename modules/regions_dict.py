e04449b3a18fc7871fd54cd643ab7a8608fbea53
Katja Kustura <kustura@geoville.com>
2025-01-23 11:54:26 +0100

    Removed options from regions_dict

--- modules/regions_dict.py
+++ modules/regions_dict.py
@@ -1,5 +1,5 @@
 regions_dict = {"Innsbruck": [(47.27, 11.41), 11, 'STATISTIK_AUSTRIA_GEM_20240101_Innsbruck.shp', 'Innsbruck'],
-                "Vienna": [(48.21, 16.37), 10, 'STATISTIK_AUSTRIA_GEM_20240101_Wien.shp', 'Wien'], 
-                "Lower Austria": [(48.41, 15.55), 8, 'BEV_VGD_Bundeslaender_NiederOesterreich.shp', 'NiederOesterreich'],
-                "all (NOT recommended)": []
+                "Vienna": [(48.21, 16.37), 10, 'STATISTIK_AUSTRIA_GEM_20240101_Wien.shp', 'Wien'] 
+                #"Lower Austria": [(48.41, 15.55), 8, 'BEV_VGD_Bundeslaender_NiederOesterreich.shp', 'NiederOesterreich'],
+                #"all (NOT recommended)": []
 }
